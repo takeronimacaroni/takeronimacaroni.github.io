826e40278add436ab5d3735a8a14357ff22e8469
takeronimacaroni <takeruyasunaga@gmail.com>
2019-07-20 22:26:49 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
 var john = [89, 120, 103];
 var mike = [116, 94, 123];
 
-johnSum = function(john){
-  return john.reduce(function(a,b){
-    return a + b
-  }, 0);
-}
 
-  console.log("johnSum");
+
+  console.log('hello world');
