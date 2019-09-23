9fff8accc92b424e30cd701a1147f4aa6573443d
takeronimacaroni <39475083+takeronimacaroni@users.noreply.github.com>
2019-09-23 16:41:37 -0400

    css animation start

--- js/script.js
+++ js/script.js
@@ -39,8 +39,8 @@ const projects = [{
 
 function sortProj(projTag) {
   for (i = 0; i < projects.length; i++) {
-    const projNum = i + 1;
-    const thisBlock = document.getElementById('proj' + projNum);
+    let projNum = i + 1;
+    let thisBlock = document.getElementById('proj' + projNum);
     thisBlock.setAttribute('style', 'display:none;');
     if (projects[i].soft.includes(projTag)) {
       thisBlock.setAttribute('style', 'display:block;');
@@ -50,42 +50,11 @@ function sortProj(projTag) {
 
 function sortLang(langTag) {
     for (i = 0; i < projects.length; i++) {
-      const projNum = i + 1;
-      const thisBlock = document.getElementById('proj' + projNum);
+      let projNum = i + 1;
+      let thisBlock = document.getElementById('proj' + projNum);
       thisBlock.setAttribute('style', 'display:none;');
       if (projects[i].lang.includes(langTag)) {
         thisBlock.setAttribute('style', 'display:block;');
       };
     };
   };
-
-
-
-
-/*
-  function sortProj(projTag) {
-    const listOfSoft = ['Ai', 'Ps', 'Ae', 'Pr', 'Xd']
-    const listOfLang = ['html', 'css', 'js', 'vue']
-    const result = []
-    const projBlock = document.querySelectorAll('.project-block');
-
-    for(i = 0; i < projects.length; i++) {
-      const projNum = i + 1;
-
-      if (listOfSoft.includes(projTag)) {
-        if (projects[i].soft.includes(projTag)) {
-          result.push(projects[i].title);
-        }} else {
-        if (projects[i].lang.includes(projTag)) {
-          result.push(projects[i].title);
-        }
-      };
-    };
-
-    $(funtion(result) {
-
-    })
-
-    console.log(result);
-  };
-*/
